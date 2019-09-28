e4f273f6f231621cd8087590e68cf676de532db3
Vyacheslav Sandrikov <sandrikov.viacheslav@gmail.com>
2019-09-28 13:31:46 +0300

    code prettify

--- test/hoisting.js
+++ test/hoisting.js
@@ -1,40 +1,44 @@
-const expect = require('chai').expect;
+const expect = require("chai").expect;
 
-describe('hoisting', () => {
-
-  it('using var variable before initialize', () => {
+describe("hoisting", () => {
+  it("using var variable before initialize", () => {
     console.log(x);
+    expect(x).to.be.eq(undefined);
     var x = 1;
   });
 
-
-  it('using function before announcement', () => {
+  it("using function before announcement", done => {
     hello();
     function hello() {
-      console.log('world')
+      console.log("hello fn used");
+      done();
     }
-  })
+  });
 
-  it('using const arrow function before announcement', () => {
-    //const dont hoist
-    fn();
-    const fn = () => console.log('foo');
-  })
-
-  it('using var arrow function before announcement', () => {
-    //dont hoist in this situation neither
-    fn();
-    var fn = () => console.log('foo');
-  })
-
-  it('hoisting in for with var', () => {
-
-    const arr = [1, 2, 3, 4]
-    for (var i = 0; i < 3; i++) {
-
+  it("using const arrow function before announcement", done => {
+    try {
+      //const don't hoist
+      fn();
+      const fn = () => console.log("foo");
+    } catch (error) {
+      done();
     }
+  });
 
-    expect(arr[i]).to.equal(4)
-  })
+  it("using var arrow function before announcement", done => {
+    try {
+      //don't hoist in this situation neither
+      fn();
+      var fn = () => console.log("foo");
+    } catch (error) {
+      done();
+    }
+  });
 
-})
+  it("hoisting in for with var", () => {
+    const arr = [1, 2, 3, 4];
+    for (var i = 0; i < 3; i++) {}
+
+    expect(arr[i]).to.equal(4);
+  });
+});
